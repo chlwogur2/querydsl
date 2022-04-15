960a1df7ef631fd0ab26bc02f912bdc2caf435e0
chlwogur2 <79786841+chlwogur2@users.noreply.github.com>
2022-04-15 18:41:21 +0900

    Domain 생성 및 Test

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,52 @@
+package study.querydsl.entity;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@Transactional
+
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1",10,teamA);
+        Member member2 = new Member("member2",15,teamB);
+        Member member3 = new Member("member3",20,teamA);
+        Member member4 = new Member("member4",25,teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // 초기화
+        em.flush();
+        em.clear();
+
+        // 확인
+        List<Member> memberList = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        assertThat(memberList.size()).isEqualTo(4);
+
+    }
+}
